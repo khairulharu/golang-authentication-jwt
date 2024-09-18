e8ad0d336f5996ebf581e00317a38a2cd61e5ce3
khairulharu <khairulaswad719@gmail.com>
2024-09-18 14:35:38 +0800

    feat: add endpoint for login

--- internal/api/user.go
+++ internal/api/user.go
@@ -33,3 +33,19 @@ func (api *apiUser) SignUp(ctx *fiber.Ctx) error {
 
 	return ctx.Status(int(response.Code)).JSON(response)
 }
+
+func (api *apiUser) LogIn(ctx *fiber.Ctx) error {
+	var request dto.LogInRequest
+
+	if err := ctx.BodyParser(&request); err != nil {
+		return ctx.Status(400).JSON(dto.Response{
+			Code:    400,
+			Message: "Body Parser Error: might body dosn fill",
+			Error:   err.Error(),
+		})
+	}
+
+	response := api.userService.LogIn(ctx.Context(), request)
+
+	return ctx.Status(int(response.Code)).JSON(response)
+}
